264885bce3bbd756a2b2775ae894abc97d8a5941
saerang <lsg10119@gmail.com>
2021-11-16 18:56:47 +0900

    Add TODO comments

--- src/main/java/com/saerang/currencyconverter/api/CurrencyConverterApiController.java
+++ src/main/java/com/saerang/currencyconverter/api/CurrencyConverterApiController.java
@@ -4,7 +4,10 @@ import com.saerang.currencyconverter.app.CurrencyConverterService;
 import com.saerang.currencyconverter.common.CommonUtils;
 import com.saerang.currencyconverter.domain.CurrencyId;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
@@ -14,6 +17,7 @@ public class CurrencyConverterApiController {
 
     final private CurrencyConverterService currencyConverterService;
 
+    // TODO: money 파라미터 검증 필요
     @GetMapping("/api/currencies/{currencyId}/conversion")
     public String conversion(@PathVariable("currencyId") CurrencyId currencyId, @RequestParam BigDecimal money) {
         return CommonUtils.getResponseDecimalFormat(currencyConverterService.getConvertedMoney(currencyId, money));
